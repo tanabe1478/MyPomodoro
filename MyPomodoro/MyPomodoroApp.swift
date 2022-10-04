2523de3b210fd181fc83dbaae83bcab71281e08e
tanabe1478 <tanabe.1478@gmail.com>
2022-10-05 06:48:38 +0900

    Initial Commit

---
+++ MyPomodoro/MyPomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPomodoroApp.swift
+//  MyPomodoro
+//
+//  Created by tanabe.nobuyuki on 2022/10/05.
+//
+
+import SwiftUI
+
+@main
+struct MyPomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
