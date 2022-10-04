626145bfcc5a0c7d69596e82f20881397e6c42f5
tanabe1478 <tanabe.1478@gmail.com>
2022-10-05 08:53:25 +0900

    initial commit

--- MyPomodoro/ContentView.swift
+++ MyPomodoro/ContentView.swift
@@ -8,14 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var pomodoroModel: PomodoroModel
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        HomeView()
+            .environmentObject(pomodoroModel)
     }
 }
 
--- MyPomodoro/MyPomodoroApp.swift
+++ MyPomodoro/MyPomodoroApp.swift
@@ -9,9 +9,36 @@ import SwiftUI
 
 @main
 struct MyPomodoroApp: App {
+    // MARK: Since We're doing Background fetching Initializing Here
+    @StateObject var pomodoroModel: PomodoroModel = .init()
+    // MARK: Scene Phase
+    @Environment(\.scenePhase) var phase
+    // MARK: Storing Last Time Stamp
+    @State var lastActiveTimeStamp: Date = Date()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(pomodoroModel)
+        }
+        .onChange(of: phase) { newValue in
+            if pomodoroModel.isStarted  {
+                if newValue == .background {
+                    lastActiveTimeStamp = Date()
+                }
+                
+                if newValue == .active {
+                    // MARK: Finding the Difference
+                    let currentTimeStampDiff = Date().timeIntervalSince(lastActiveTimeStamp)
+                    if pomodoroModel.totalSeconds - Int(currentTimeStampDiff) <= 0 {
+                        pomodoroModel.isStarted = false
+                        pomodoroModel.totalSeconds = 0
+                        pomodoroModel.isFinished = true
+                    } else {
+                        pomodoroModel.totalSeconds -= Int(currentTimeStampDiff)
+                    }
+                }
+            }
         }
     }
 }
